1f3d45d3a5e5778a5cb10377c13518a993161f3d
grewn0uille <lucieanglade.gm@gmail.com>
2017-05-24 14:58:26 +0200

    Add makefile and setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+tests_requirements = [
+    'pytest',
+    'pytest-cov',
+    'pytest-flake8',
+    'pytest-isort',
+]
+
+setup(
+    name='roadhog',
+    author='Kozea',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'requests',
+    ],
+    tests_require=tests_requirements,
+    extras_require={'test': tests_requirements}
+)
