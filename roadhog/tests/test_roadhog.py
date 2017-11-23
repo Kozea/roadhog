496912d74f0419442aabd7ab012309ed5c592071
grewn0uille <lucieanglade.gm@gmail.com>
2017-11-23 14:39:53 +0100

    fix test

--- roadhog/tests/test_roadhog.py
+++ roadhog/tests/test_roadhog.py
@@ -15,7 +15,7 @@ def test_api(http, alembic_config):
     assert http.get('/api/project')[0] == 200
     assert http.get('/api/commit_')[0] == 200
     assert http.get('/api/job')[0] == 200
-    assert http.get('/api/branch')[0] == 200
+    assert http.get('/api/branche')[0] == 200
 
 
 def test_redirect_to(http):
@@ -43,19 +43,21 @@ def test_build_project_from_pipeline(json_content_pipeline_hook):
 def test_build_commit_from_push(json_content_push_hook):
     branch = json_content_push_hook['ref'].split('/')[-1]
     project_id = json_content_push_hook['project_id']
-    assert roadhog.build_project(
-        json_content_push_hook['commits'][0], branch, project_id) == {
-        'id': '1234abcd',
-        'branch': 'phoenix_2223',
-        'message': 'message',
-        'author': 'Juste LeBlanc',
-        'commit_date': '2017-10-10 15:55:08 UTC',
-        'project_id': 1320772
+    assert roadhog.build_commit_from_push(
+        json_content_push_hook['commits'][0],
+        branch=branch, project_id=project_id) == {
+            'id': '1234abcd',
+            'branch': 'phoenix_2223',
+            'message': 'message',
+            'author': 'Juste LeBlanc',
+            'commit_date': datetime.datetime(2017, 10, 10, 15, 55, 8,
+                                             tzinfo=datetime.timezone.utc),
+            'project_id': 1320772
     }
 
 
 def test_build_commit_from_pipeline(json_content_pipeline_hook):
-    assert roadhog.build_commit(json_content_pipeline_hook) == {
+    assert roadhog.build_commit_from_pipeline(json_content_pipeline_hook) == {
         'pipeline_id': 9632758,
         'branch': 'phoenix_2223',
         'id': '1234abcd',
@@ -94,7 +96,7 @@ def test_build_job(json_content_pipeline_hook, json_headers):
 def test_add_data(app, db_session, json_content_pipeline_hook, json_headers):
     with app.test_request_context():
         app.preprocess_request()
-        roadhog.master(json_content_pipeline_hook, json_headers, 'build')
+        roadhog.master(json_content_pipeline_hook, json_headers, 'Build Hook')
         roadhog.add_data(
             Job, json_content_pipeline_hook['build_id'], {'log': 'some logs'})
         assert (
@@ -108,18 +110,21 @@ def test_master(
         json_content_update_pipeline_hook, json_headers):
     with app.test_request_context():
         app.preprocess_request()
-        roadhog.master(json_content_push_hook, json_headers, 'push')
+        roadhog.master(json_content_push_hook, json_headers, 'Push Hook')
         assert db_session.query(Commit).first() is not None
         assert db_session.query(Project).first() is not None
-        roadhog.master(json_content_pipeline_hook, json_header, 'build')
+        roadhog.master(json_content_pipeline_hook, json_headers, 'build')
         assert db_session.query(Job).first() is not None
         roadhog.master(
             json_content_update_pipeline_hook, json_headers, 'build')
         assert (
             db_session.query(Project)
-            .filter(Project.id == json_content_update['project_id'])
+            .filter(
+                Project.id == json_content_update_pipeline_hook['project_id'])
             .first().name == 'change name')
         assert (
-            db.session.query(Commit)
-            .filter(Commit.project_id == json_content_update['project_id'])
+            db_session.query(Commit)
+            .filter(
+                Commit.project_id ==
+                json_content_update_pipeline_hook['project_id'])
             .first().commit_date != '')
